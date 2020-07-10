e44d5d8b307669e4b07961580920f8a63620c4a1
Mohammad Hammoud <mohammadhammoud1921@gmail.com>
2020-07-10 15:37:51 +0300

    Revert "Revert "Bug  Fix""
    This reverts commit 0ad6f525245ae7857f32274fbdac9fe56be0fbfa.

---
+++ app/src/main/java/com/pkg/mychatapplication/FragmentsAdapter.java
@@ -0,0 +1,35 @@
+package com.pkg.mychatapplication;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+class FragmentsAdapter extends FragmentPagerAdapter {
+    public FragmentsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return  requestsFragment;
+            case 1:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return  chatsFragment;
+            case 2:
+                friendsFragment friendsFragment = new friendsFragment();
+                return  friendsFragment;
+
+                default:
+                    return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;   //number of tabs I have
+    }
+}
