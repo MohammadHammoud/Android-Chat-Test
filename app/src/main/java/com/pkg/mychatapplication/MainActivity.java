71fa7dfab2bf61ab340f59c937b0d0e48ffe8f25
Mohammad Hammoud <mohammadhammoud1921@gmail.com>
2020-07-10 14:07:29 +0300

    feat
    Added the logout feature.

--- app/src/main/java/com/pkg/mychatapplication/MainActivity.java
+++ app/src/main/java/com/pkg/mychatapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pkg.mychatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,8 +16,27 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
     //This Page is the chatPage but we have to do some authentication so that the user don't end up here without having logged in
     private FirebaseAuth mAuth;
-    private FirebaseUser mCurrentUser;
+
     private Toolbar mToolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_menu,menu);
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if(item.getItemId()== R.id.main_logout_btn){       //Clicked on the logout button
+             mAuth.signOut();
+             goToSignUp();      //update the UI
+
+         }
+         return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-        mCurrentUser = mAuth.getCurrentUser();
         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Chat");
@@ -30,12 +52,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if(mCurrentUser==null){ //Go back to SignUp page
+        FirebaseUser mCurrentUser = mAuth.getCurrentUser();
+        if(mCurrentUser==null) { //Go back to SignUp page
+            goToSignUp();
+        }
+
+    }
+    public void onBackPressed() {
+        // super.onBackPressed();
+        Toast.makeText(MainActivity.this,"There is no back action",Toast.LENGTH_LONG).show();
+        return;
+    }
+    public void goToSignUp(){
             Intent login = new Intent(MainActivity.this, SignUpPage.class);
             login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(login);
             finish();
-        }
+
     }
+
 }
